784489f7a6e944226b4670783a5d5be8382d734a
juansefdz <jsfm81091@gmail.com>
2024-06-19 20:42:34 -0500

    01. iniciando con js alura

---
+++ HTML/primer desafio html js/app.js
@@ -0,0 +1,17 @@
+alert ("¡Bienvenida y bienvenido a nuestro sitio web!");
+
+let nombre = "Luna";
+let edad = 25;
+
+let numeroVentas=50;
+let saldoDisponible=1000;
+
+alert("¡Error! Completa todos los campos");
+
+let mensajeDeError  = "¡Error! Completa todos los campos desde variable";
+alert (mensajeDeError);
+
+let nombre2=prompt("ingrese el nombre del usuario");
+let edad2=prompt("ingrese la edad del usuario " + nombre2 +": " );
+
+if (edad2>=18) alert("¡Puedes obtener tu licencia de conducir!");
---
+++ HTML/primer proyecto HTML/app.js
@@ -0,0 +1,9 @@
+
+let palabra = prompt("mensaje en una variable");
+alert (palabra);
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto) {
+    alert('Acertaste el numero');
+}
