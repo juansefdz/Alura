89c2785bffc838f0f9a8367c1cb6fa81995d45d7
juansefdz <jsfm81091@gmail.com>
2024-06-24 20:01:11 -0500

    update

--- HTML/primer proyecto HTML/app.js
+++ HTML/primer proyecto HTML/app.js
@@ -1,9 +1,25 @@
+let numeroSecreto = Math.floor(Math.random()*rangoMayor)+1;
+let numeroUsuario;
+let contador=1;
+let maximoIntentos=4;
 
-let palabra = prompt("mensaje en una variable");
-alert (palabra);
+let rangoMayor= prompt("ingrese el rango mayor");
 
-console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = parseInt(prompt(`dime un numero entre 1 y ${rangoMayor}: `));
+
+  console.log(numeroUsuario);
+
+  if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste el numero era ${numeroUsuario}. en ${intentos} ${intentos==1?"vez":"veces"}`);
+    
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("el numero secreto es menor");
+    }
+    alert("el numero es mayor");
+    contador++;
+  }
+  
 }
